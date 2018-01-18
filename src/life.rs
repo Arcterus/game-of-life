cf117d6701b64118bdf038b66cfe55ecd68f5db2
Alex Lyon <arcterus@mail.com>
2018-01-18 00:17:59 -0800

    Implement incomplete zooming and remove some allocations

--- src/life.rs
+++ src/life.rs
@@ -1,13 +1,15 @@
+#![feature(conservative_impl_trait)]
+
 #[macro_use]
 extern crate clap;
-
+extern crate fnv;
 extern crate graphics;
 extern crate piston;
-
+extern crate fps_counter;
 extern crate glutin_window;
 extern crate opengl_graphics;
 
-use std::collections::HashSet;
+use fnv::FnvHashSet as HashSet;
 use clap::Arg;
 use graphics::*;
 use opengl_graphics::{GlGraphics, OpenGL};
@@ -16,6 +18,7 @@ use piston::event_loop::*;
 use piston::input::*;
 use piston::window::AdvancedWindow;
 use glutin_window::GlutinWindow;
+use fps_counter::FPSCounter;
 
 pub const WINDOW_TITLE: &'static str = "Conway's Game of Life";
 
@@ -31,6 +34,8 @@ pub enum Neighbor {
 }
 
 pub struct Grid {
+   width: usize,
+   height: usize,
    grid: Vec<Vec<Option<Block>>>,
    blocks: HashSet<Block>
 }
@@ -47,11 +52,13 @@ pub struct Location {
 }
 
 pub struct App {
+   fps: FPSCounter,
    width: u32,
    height: u32,
    gl: GlGraphics,
    grid: Grid,
    started: bool,
+   zoom: f64,
    mouse_loc: (f64, f64),
    mouse_down: (bool, bool)   // (left, right)
 }
@@ -64,8 +71,10 @@ impl Grid {
          rows.push(vec![None; width / BLOCK_SIZE]);
       }
       Grid {
+         width: width,
+         height: height,
          grid: rows,
-         blocks: HashSet::new()
+         blocks: HashSet::default()
       }
    }
 
@@ -91,55 +100,54 @@ impl Grid {
       }
    }
 
-   pub fn neighbors(&self, block: &Block) -> Vec<Neighbor> {
-      let mut vec = vec!();
+   pub fn neighbors<'a>(&'a self, block: &'a Block) -> Option<impl Iterator<Item = Neighbor> + 'a> {
       if self.valid(block.loc.x, block.loc.y) {
-         let places = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (0, -1), (1, 1), (1, -1), (1, 0)];
-         for &(x, y) in &places {
+         const PLACES: [(isize, isize); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (0, -1), (1, 1), (1, -1), (1, 0)];
+         
+         Some(PLACES.into_iter().filter_map(move |&(x, y)| {
             let xpos = (block.loc.x as isize + x) as usize;
             let ypos = (block.loc.y as isize + y) as usize;
             if self.valid(xpos, ypos) {
-               let row = &self.grid[ypos];
-               vec.push(match row[xpos] {
+               Some(match self.grid[ypos][xpos] {
                   Some(blk) => Neighbor::Block(blk),
                   None => Neighbor::Location(Location::new(xpos, ypos))
-               });
+               })
+            } else {
+               None
             }
-         }
-      }
-      vec
-   }
-
-   #[inline]
-   pub fn live_neighbors(&self, block: &Block) -> Vec<Block> {
-      let mut live = vec!();
-      for neighbor in self.neighbors(block) {
-         match neighbor {
-            Neighbor::Block(blk) => live.push(blk),
-            _ => {}
-         }
-      }
-      live
-   }
-
-   #[inline]
-   pub fn dead_neighbors(&self, block: &Block) -> Vec<Location> {
-      let mut dead = vec!();
-      for neighbor in self.neighbors(block) {
-         match neighbor {
-            Neighbor::Location(loc) => dead.push(loc),
-            _ => {}
-         }
-      }
-      dead
-   }
-
-   pub fn contains(&self, block: &Block) -> bool {
-      if self.valid(block.loc.x, block.loc.y) {
-         self.grid[block.loc.y][block.loc.x].is_some()
+         }))
       } else {
-         false
+         None
       }
+   }
+
+   #[inline]
+   pub fn live_neighbors<'a>(&'a self, block: &'a Block) -> impl Iterator<Item = Block> + 'a {
+      self.neighbors(block).map(|neighbors| {
+         neighbors.into_iter().filter_map(|neighbor| {
+            match neighbor {
+               Neighbor::Block(blk) => Some(blk),
+               _ => None
+            }
+         })
+      }).into_iter().flat_map(|elem| elem)
+   }
+
+   #[inline]
+   pub fn dead_neighbors<'a>(&'a self, block: &'a Block) -> impl Iterator<Item = Location> + 'a {
+      self.neighbors(block).map(|neighbors| {
+         neighbors.into_iter().filter_map(|neighbor| {
+            match neighbor {
+               Neighbor::Location(loc) => Some(loc),
+               _ => None
+            }
+         })
+      }).into_iter().flat_map(|elem| elem)
+   }
+
+   #[inline]
+   pub fn contains(&self, block: &Block) -> bool {
+      self.valid(block.loc.x, block.loc.y) && self.grid[block.loc.y][block.loc.x].is_some()
    }
 
    #[inline]
@@ -148,24 +156,38 @@ impl Grid {
    }
 
    #[inline]
-   pub fn render(&self, gl: &mut GlGraphics, started: bool, args: &RenderArgs) {
+   pub fn render(&mut self, gl: &mut GlGraphics, started: bool, zoom_factor: f64, args: &RenderArgs) {
       use graphics::grid::*;
       use graphics::line::Line;
 
       const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
+      // FIXME: embed the width stuff in the height section
+      if self.height as f64 / zoom_factor > self.grid.len() as f64 {
+         let len = self.grid[0].len();
+         for _ in 0..((self.height as f64 * zoom_factor).round() as u64) {
+            self.grid.push(vec![None; len]);
+         }
+      }
+      if self.width as f64 / zoom_factor > self.grid[0].len() as f64 {
+         let extension = vec![None; (self.width as f64 * zoom_factor).round() as usize];
+         for row in &mut self.grid {
+            row.extend(extension.iter())
+         }
+      }
+
       if !started {
          gl.draw(args.viewport(), |c, gl| {
             Grid {
-               cols: self.grid[0].len() as u32,
-               rows: self.grid.len() as u32,
-               units: BLOCK_SIZE as f64
+               cols: (self.grid[0].len() as f64 * zoom_factor) as u32,
+               rows: (self.grid.len() as f64 * zoom_factor) as u32,
+               units: (BLOCK_SIZE as f64) * zoom_factor
             }.draw(&Line::new(BLACK, 1.0), &c.draw_state, c.transform, gl);
          });
       }
 
       for block in &self.blocks {
-         block.render(gl, args);
+         block.render(gl, zoom_factor, args);
       }
    }
 }
@@ -179,18 +201,18 @@ impl Block {
    }
 
    #[inline]
-   pub fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+   pub fn render(&self, gl: &mut GlGraphics, zoom_factor: f64, args: &RenderArgs) {
       use graphics::*;
 
       const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-      let block = rectangle::square(0.0, 0.0, BLOCK_SIZE as f64);
+      let block = rectangle::square(0.0, 0.0, BLOCK_SIZE as f64 * zoom_factor);
 
       gl.draw(args.viewport(), |c, gl| {
          rectangle(BLACK,
                    block,
-                   c.transform.trans((self.loc.x * BLOCK_SIZE) as f64,
-                                     (self.loc.y * BLOCK_SIZE) as f64),
+                   c.transform.trans((self.loc.x * BLOCK_SIZE) as f64 * zoom_factor,
+                                     (self.loc.y * BLOCK_SIZE) as f64 * zoom_factor),
                    gl);
       });
    }
@@ -210,11 +232,13 @@ impl App {
    #[inline]
    pub fn new(width: u32, height: u32, gl: GlGraphics) -> App {
       App {
+         fps: FPSCounter::new(),
          width: width,
          height: height,
          gl: gl,
          grid: Grid::new(width as usize, height as usize),
          started: false,
+         zoom: 0.0,
          mouse_loc: (0.0, 0.0),
          mouse_down: (false, false)
       }
@@ -282,12 +306,17 @@ impl App {
       self.mouse_paint();
    }
 
+   pub fn mouse_scroll(&mut self, pos: [f64; 2]) {
+      self.zoom += pos[1];
+   }
+
    fn mouse_paint(&mut self) {
       let (left, right) = self.mouse_down;
       if !self.started && (left || right) {
          let (mut x, mut y) = self.mouse_loc;
-         x = (x - (x as usize % BLOCK_SIZE) as f64) / BLOCK_SIZE as f64;
-         y = (y - (y as usize % BLOCK_SIZE) as f64) / BLOCK_SIZE as f64;
+         x = (x - (x as usize % ((BLOCK_SIZE as f64 * self.zoom_factor())) as usize) as f64) / (BLOCK_SIZE as f64 * self.zoom_factor());
+         y = (y - (y as usize % ((BLOCK_SIZE as f64 * self.zoom_factor())) as usize) as f64) / (BLOCK_SIZE as f64 * self.zoom_factor());
+
          let block = Block::new(Location::new(x as usize, y as usize));
          if left {
             self.grid.insert(block);
@@ -297,14 +326,15 @@ impl App {
       }
    }
 
+   // FIXME: this is now out of date due to zooming
    #[cfg(random)]
    #[inline]
    fn update_logic(&mut self) {
       use std::rand::random;
 
-      let mut x = (random::<f64>() * self.width as f64) as u32;
+      let mut x = (random() * self.width as f64) as u32;
       x = (x - x % BLOCK_SIZE) / BLOCK_SIZE;
-      let mut y = (random::<f64>() * self.height as f64) as u32;
+      let mut y = (random() * self.height as f64) as u32;
       y = (y - y % BLOCK_SIZE) / BLOCK_SIZE;
       self.grid.insert(Block::new(Location::new(x, y)));
    }
@@ -314,25 +344,34 @@ impl App {
    fn update_logic(&mut self) {
       let mut remove = vec!();
       let mut add = vec!();
-      
+
       for &block in &self.grid.blocks {
-         let live = self.grid.live_neighbors(&block);
-         let livelen = live.len();
-         if livelen != 2 && livelen != 3 {
+         let live = self.grid.live_neighbors(&block).count();
+         if live != 2 && live != 3 {
             remove.push(block);
          }
-         for loc in self.grid.dead_neighbors(&block) {
+         add.extend(self.grid.dead_neighbors(&block).filter_map(|loc| {
             let block = Block::new(loc);
-            if self.grid.live_neighbors(&block).len() == 3 {
-               add.push(block);
+            if self.grid.live_neighbors(&block).count() == 3 {
+               Some(block)
+            } else {
+               None
             }
-         }
+         }));
       }
       for block in remove {
          self.grid.remove(&block);
       }
       for block in add {
          self.grid.insert(block);
+      }
+   }
+
+   fn zoom_factor(&self) -> f64 {
+      if self.zoom < 0.0 {
+         1.0 / (-self.zoom + 1.0)
+      } else {
+         self.zoom + 1.0
       }
    }
 
@@ -343,12 +382,15 @@ impl App {
    }
 
    pub fn render(&mut self, args: &RenderArgs) {
-      (&mut self.gl).viewport(0, 0, args.width as i32, args.height as i32);
+      println!("{}", self.fps.tick());
+
+      self.gl.viewport(0, 0, args.width as i32, args.height as i32);
       self.gl.draw(args.viewport(), |_, gl| {
          clear([1.0, 1.0, 1.0, 1.0], gl);
       });
 
-      self.grid.render(&mut self.gl, self.started, args);
+      let factor = self.zoom_factor();
+      self.grid.render(&mut self.gl, self.started, factor, args);
    }
 }
 
@@ -366,7 +408,7 @@ fn main() {
                                     .value_name("SPEED")
                                     .help("Sets the speed of each update")
                                     .takes_value(true)
-                                    .validator(is_valid_speed))
+                                    .validator(is_positive))
                                .arg(Arg::with_name("width")
                                     .short("w")
                                     .long("width")
@@ -381,6 +423,14 @@ fn main() {
                                     .help("Sets the height of the window")
                                     .takes_value(true)
                                     .validator(is_valid_width_or_height))
+                               // FIXME: set up so the value is used
+                               .arg(Arg::with_name("block")
+                                    .short("b")
+                                    .long("block")
+                                    .value_name("SIZE")
+                                    .help("Sets the size of the blocks")
+                                    .takes_value(true)
+                                    .validator(is_positive))
                                .get_matches();
 
    let speed = matches.value_of("speed").map(|valstr| valstr.parse::<u64>().unwrap())
@@ -390,6 +440,7 @@ fn main() {
    let height = matches.value_of("height").map(|valstr| valstr.parse::<u32>().unwrap())
                                           .unwrap_or(DEFAULT_WINDOW_HEIGHT);
 
+   // TODO: switch to gfx-rs backend
    let opengl = OpenGL::V3_2;
 
    let mut window: GlutinWindow = WindowSettings::new(
@@ -399,7 +450,7 @@ fn main() {
 
    let mut app = App::new(width, height, GlGraphics::new(opengl));
 
-   let event_settings = EventSettings::new().ups(speed).max_fps(60);
+   let event_settings = EventSettings::new().ups(speed);
    let mut events = Events::new(event_settings);
    while let Some(e) = events.next(&mut window) {
       if let Some(r) = e.render_args() {
@@ -421,6 +472,10 @@ fn main() {
       if let Some(c) = e.mouse_cursor_args() {
          app.mouse_move(c);
       }
+
+      if let Some(s) = e.mouse_scroll_args() {
+         app.mouse_scroll(s);
+      }
    }
 }
 
@@ -434,10 +489,10 @@ fn is_valid_width_or_height(valstr: String) -> Result<(), String> {
    Err(format!("{} is not a valid size (must be positive and divisible by {}", valstr, BLOCK_SIZE))
 }
 
-fn is_valid_speed(valstr: String) -> Result<(), String> {
+fn is_positive(valstr: String) -> Result<(), String> {
    if valstr.parse::<u32>().is_ok() {
       Ok(())
    } else {
-      Err(format!("{} is not a valid speed (must be positive)", valstr))
+      Err(format!("{} is not a valid number (must be positive)", valstr))
    }
 }
